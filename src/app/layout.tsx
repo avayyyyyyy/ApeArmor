4db9e6c4b148e513461193c71cb80ee2b9c116b3
avayyyyyyy <jabhi465@gmail.com>
2024-05-18 20:46:12 +0530

    🐞 fix: Theming Issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <ProgressBarContext>
             <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="light"
               enableSystem
               disableTransitionOnChange
             >
